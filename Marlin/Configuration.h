7309b17fd81f86008106120441428bb1aca2f4c2
csn495 <calvinn495@gmail.com>
2021-08-15 22:09:43 -0400

    Changed bed size to Y=208 to reflect H2 install

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1341,7 +1341,7 @@
 
 // The size of the printable area
 #define X_BED_SIZE 220
-#define Y_BED_SIZE 220
+#define Y_BED_SIZE 208
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
